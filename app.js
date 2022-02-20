aedd8914efdd4492cd37fa009c5290f292358614
Sergio Abadia <sergiodanielabadia@gmail.com>
2022-02-20 14:45:23 -0300

    update logo-home

--- app.js
+++ app.js
@@ -8,10 +8,10 @@ app.use(express.static(publicPath));
 
 app.get('/', (req,res) => res.sendFile(path.join(__dirname,'/views/home.html')));
 app.get('/register', (req,res) => res.sendFile(path.join(__dirname,'/views/register.html')));
+app.get('/home', (req,res) => res.sendFile(path.join(__dirname,'/views/home.html')));
 app.get('/login', (req,res) => res.sendFile(path.join(__dirname,'/views/login.html')));
 app.post('/register', (req,res) =>res.sendFile(path.join(__dirname,'/views/home.html')));
 app.post('/login', (req,res) =>res.sendFile(path.join(__dirname,'/views/home.html')));
-
 
 
 app.listen(process.env.PORT || 4000, () => {console.log(`server running at localhost:${port}`)});
